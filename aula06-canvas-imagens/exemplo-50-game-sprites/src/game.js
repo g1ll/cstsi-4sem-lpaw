c24bb2c789d02510e27b3b1b5cb2722f7b406cfb
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-09-04 19:02:56 -0300

    feat: exemplo de sprites no jogo com classe Hero

---
+++ aula06-canvas-imagens/exemplo-50-game-sprites/src/game.js
@@ -0,0 +1,70 @@
+import Enemy from "./Enemy"
+import Smile from "./Smile"
+import { keyPress, key } from "./keyboard"
+import Hero from "./Hero"
+
+let CTX
+let CANVAS
+const FRAMES = 60
+
+const qtdEnemies = 1
+
+let enemies = Array.from({length:qtdEnemies});
+
+const smile = new Smile(300, 100, 20, 5, 'yellow')
+
+const hero = new Hero(310,100,20,5,82,89,'img/goblin.png',FRAMES)
+
+let gameover = false
+let anime;
+let boundaries
+
+const init = () => {
+	console.log("Initialize Canvas")
+	CANVAS = document.querySelector('canvas')
+	CTX = CANVAS.getContext('2d')
+	
+	boundaries = {
+		width: CANVAS.width,
+		height: CANVAS.height
+	}
+
+	enemies = enemies.map(i=>new Enemy(
+			Math.random()*CANVAS.width,
+			Math.random()*CANVAS.height,
+			10, 5, 'red'
+		))
+	
+	keyPress(window)
+	loop()
+}
+
+const loop = () => {
+	setTimeout(() => {
+
+		CTX.clearRect(0, 0, CANVAS.width, CANVAS.height)
+
+		smile.move(boundaries, key)
+		smile.paint(CTX)
+
+		hero.move(boundaries, key)
+		hero.draw(CTX)
+		
+		enemies.forEach(e =>{
+			e.move(boundaries, 0) 
+			e.draw(CTX)
+			 //var = teste?verdadeiro:falso;
+			 gameover = !gameover 
+			 		? hero.colide(e)
+					: true;
+		})
+	
+		if (gameover) {
+			console.error('DEAD!!!')
+			cancelAnimationFrame(anime)
+		} else	anime = requestAnimationFrame(loop)
+
+	}, 1000 / FRAMES)
+}
+
+export { init }
