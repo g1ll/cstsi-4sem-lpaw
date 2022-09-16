78c380f20eab86c72ac939fff242f29f5ad75161
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-09-16 13:16:46 -0300

    feat: alteracoes feitas em aula

--- aula08-webgl-threejs/exemplo-54-usando-textura/src/main.js
+++ aula08-webgl-threejs/exemplo-54-usando-textura/src/main.js
@@ -12,15 +12,16 @@ const camera = new THREE.PerspectiveCamera(
 	75, //campo de visao vertical
 	aspecto, //aspecto da imagem (Largura/Altura)
 	0.1, //Plano proximo
-	100//Plano distante
+	200//Plano distante
 );
-camera.position.z = 35
+camera.position.z = 100
 
 
 const geometry = new THREE.BoxGeometry(20, 20, 20)
 const texture = new THREE.TextureLoader()
-						.load('img/crate.jpg',animate);
-const material = new THREE.MeshBasicMaterial({ map: texture });
+			.load('img/crate.jpg',animate);
+const material = new THREE.MeshBasicMaterial(
+	{ map: texture });
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
