f59889b772099f32f15643c6b6f18a19906ffedc
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-12-29 10:25:00 -0300

    refact: adicioando o resize de tela e controles. Controles do avaiao so funcionam sem botao do mouse pressionado

--- aula11-threejs-controls-skybox/exemplo-61-skybox-texture/src/main.js
+++ aula11-threejs-controls-skybox/exemplo-61-skybox-texture/src/main.js
@@ -4,7 +4,7 @@ import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
 import { createSkyBox } from './skybox';
 
-const renderer = new THREE.WebGLRenderer({antialias:true})
+const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
@@ -12,19 +12,27 @@ const scene = new THREE.Scene()
 scene.background = new THREE.Color(0x000000)
 
 const camera = new THREE.PerspectiveCamera(
-  40, window.innerWidth / window.innerHeight, 0.1, 100
+  40, window.innerWidth / window.innerHeight, 0.1, 1000
 );
 
 camera.position.z = 1
 
-const controls = new OrbitControls( camera, renderer.domElement );
+//Resimensionamento da camera ao redimensionar a tela
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  console.log(`Resize: ${camera.aspect}`)
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}, false);
 
- var light = new THREE.AmbientLight(0xffffff, 10);
- scene.add(light);
+const controls = new OrbitControls(camera, renderer.domElement);
 
- var plight = new THREE.PointLight(0xffffff, 50,50);
- plight.position.set( 0, 25, -10 );
- scene.add(plight);
+var light = new THREE.AmbientLight(0xffffff, 10);
+scene.add(light);
+
+var plight = new THREE.PointLight(0xffffff, 50, 50);
+plight.position.set(0, 25, -10);
+scene.add(plight);
 
 let model
 const modelPath = 'models/f15c/'
@@ -32,7 +40,7 @@ const mtlFile = 'f15c.mtl'
 const objFile = 'f15c.obj'
 
 const manager = new THREE.LoadingManager();
-manager.onProgress = (item, loaded, total) =>{
+manager.onProgress = (item, loaded, total) => {
   console.log(item, loaded, total);
 };
 
@@ -41,25 +49,36 @@ const objLoader = new OBJLoader();
 
 mtlLoader.setPath(modelPath)
   .load(mtlFile, (materials) => {
-  materials.preload()
-  objLoader.setMaterials(materials)
-  objLoader.setPath(modelPath).load(objFile, (object) => {
-    model = object
-    model.scale.setScalar(.5)//redimensiona o objeto
-    model.position.x=.05
-    model.rotation.z=.5
-    scene.add(model)
-    createSkyBox('bluesky',70).then(sky=>{
-      console.log('sky created')
-      console.log(sky)
-      scene.add(sky)
-      animate()
-    });
+    materials.preload()
+    objLoader.setMaterials(materials)
+    objLoader.setPath(modelPath).load(objFile, (object) => {
+      model = object
+      model.scale.setScalar(.5)//redimensiona o objeto
+      model.position.x = .05
+      model.rotation.z = .5
+      scene.add(model)
+      createSkyBox('bluesky', 70).then(sky => {
+        console.log('sky created')
+        console.log(sky)
+        scene.add(sky)
+        animate()
+      });
+    })
   })
-})
 
 function animate() {
   controls.update();
   renderer.render(scene, camera)
   requestAnimationFrame(animate)
 }
+
+window.addEventListener('mousemove', event => {
+  if (!event.buttons) {
+    let wh = window.innerHeight
+    let ww = window.innerWidth
+    let my = event.clientY
+    let mx = event.clientX
+    if (model) model.rotation.x += (my - wh / 2) / wh / 100
+    if (model) model.rotation.z -= (mx - ww / 2) / ww / 100
+  }
+})
