31a2ba59c6843a2228033411695792bacda55887
Gill Velleda Gonzales <gillvelleda@gmail.com>
2023-05-06 16:24:04 -0300

    refactor: exemplo de async/await com try/catch

--- aula09-canvas-imagens/exemplo-9-2-canvas-animated-sprites/src/init.js
+++ aula09-canvas-imagens/exemplo-9-2-canvas-animated-sprites/src/init.js
@@ -5,6 +5,8 @@ let CANVAS
 const FRAMES = 6
 
 let goblinImage = null
+let bgImage = null
+let bgPattern = null
 let x = 0
 let y = 0
 
@@ -12,25 +14,37 @@ let totalSpritesX = 3	//Total de sprites em X
 let totalSpritesY = 4	//Total de sprites em Y
 let cellWidth	//largura da celular de recorte
 let cellHeight 	//altura da celula de recorte
-
+//async/await
+//loadImage().then(()=>{resolvido}).catch(rejeitado)
+//
 const init = async () => {
 	console.log("Initialize Canvas")
 	CANVAS = document.querySelector('canvas')
 	CTX = CANVAS.getContext('2d')
-	goblinImage = await loadImage('img/goblin.png')
-	
-	cellWidth = goblinImage.naturalWidth/totalSpritesX +3.5
-	cellHeight = goblinImage.naturalHeight/totalSpritesY
-	
-	loop()
-	animeSprite()
+	//goblin sprites png
+	try {
+		goblinImage = await loadImage('img/goblin.png')
+
+		//bg pattern
+		bgImage = await loadImage('img/bg/dust.png')
+		
+		bgPattern = CTX.createPattern(bgImage, 'repeat')
+		
+		cellWidth = goblinImage.naturalWidth / totalSpritesX + 3.5
+		cellHeight = goblinImage.naturalHeight / totalSpritesY
+
+		loop()
+	} catch (e) {
+		console.error("Assets Error: ${e.message}");
+	}
 }
 
 
 const loop = () => {
 
 	setTimeout(() => {
-		CTX.clearRect(0, 0, CANVAS.width, CANVAS.height)
+		CTX.fillStyle = bgPattern;
+		CTX.fillRect(0, 0, CANVAS.width, CANVAS.height)
 
 		x = x < totalSpritesX - 1 ? x + 1 : 0;
 		//var = (Teste)?verdade:falso;
@@ -41,7 +55,7 @@ const loop = () => {
 			0*cellHeight,
 			cellWidth,
 			cellHeight, //source
-			100, 10, cellWidth*2, cellHeight*2 //draw
+			200, 40, cellWidth*2, cellHeight*2 //draw
 		)
 
 		requestAnimationFrame(loop)
