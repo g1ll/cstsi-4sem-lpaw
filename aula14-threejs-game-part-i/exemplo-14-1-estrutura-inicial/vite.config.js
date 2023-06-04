aea9059e0d41c27136cf25629d71636aa318332f
Gill Velleda Gonzales <gillvelleda@gmail.com>
2023-06-04 20:31:30 -0300

    feat: novas configurações no vite aula 14

--- aula14-threejs-game-part-i/exemplo-14-1-estrutura-inicial/vite.config.js
+++ aula14-threejs-game-part-i/exemplo-14-1-estrutura-inicial/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // ...
+  esbuild: {
+    supported: {
+      'top-level-await': true //browsers can handle top-level-await features
+    },
+  },
   publicDir:'../assets'
 })
