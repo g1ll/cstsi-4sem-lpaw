3a27468006a750cdf2d12bc41d11cc63caa59b06
Gill Velleda Gonzales <gillvelleda@gmail.com>
2023-06-04 21:24:58 -0300

    fix: vite config test and rm public folder

--- aula14-threejs-game-part-i/exemplo-14-1-estrutura-inicial/vite.config.js
+++ aula14-threejs-game-part-i/exemplo-14-1-estrutura-inicial/vite.config.js
@@ -1,7 +1,7 @@
 export default {
-  esbuild: {
-    supported: {
-      'top-level-await': true //browsers can handle top-level-await features
-    },
-  }
+  build: {
+    target: 'esnext', //browsers can handle the latest ES features
+    sourcemap:true,
+  },
+  publicDir:'../assets'
 }
